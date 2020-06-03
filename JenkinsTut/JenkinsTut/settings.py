0799d54c0dbfb51f5e998e76f91b57778dfee8b0
Mohammad Albalboul <mohammad.albalboul@exalt.ps>
2020-06-03 11:31:51 +0300

    fixed comments, added jquery datatable

--- JenkinsTut/JenkinsTut/settings.py
+++ JenkinsTut/JenkinsTut/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'dashboard',
-    'rest_framework'
+    'rest_framework',
+    'dashboard'
 ]
 
 MIDDLEWARE = [
--- JenkinsTut/static/js/buildInfoTable.js
+++ JenkinsTut/static/js/buildInfoTable.js
@@ -1,17 +1,45 @@
 const apiLink = JSON.parse(document.getElementById('api').textContent);
 
 $(document).ready(function() {
-    const groupColumn = 2;
+    const groupColumn = 0;
     let tableData;
-
-    $.ajax({
+    const loadDataTable = async () => {
+        const data = await $.ajax({
             type: 'GET',
             url: apiLink,
             success: function (response) {
-                tableData =  response && response.builds;
+                tableData =  response && response.builds.reduce((acc, current) => (
+                        [...acc, ...current.subBuilds.map(sub => ({...sub, number: current.number}))]
+                    ), []);
             },
             error: function (response) {
                 console.error(`not valid and its ${response}`)
             }
         })
-} );
+        $('#data-table').DataTable( {
+            data: tableData,
+            columns: [
+                { data: "number", title: "Build Number" },
+                { data: "buildNumber", title: "Sub Build Number" },
+                { data: "duration", title: "Duration" },
+                { data: "result", title: "Result" },
+            ],
+            drawCallback: function ( settings ) {
+                const api = this.api();
+                const rows = api.rows( {page:'current'} ).nodes();
+                let last=null;
+
+                api.column(groupColumn, {page:'current'} ).data().each( function ( group, i ) {
+                    if ( last !== group ) {
+                        $(rows).eq( i ).before(
+                            '<tr class="group"><td colspan="5">'+group+'</td></tr>'
+                        );
+
+                        last = group;
+                    }
+                } );
+            }
+        });
+    }
+    loadDataTable()
+});
